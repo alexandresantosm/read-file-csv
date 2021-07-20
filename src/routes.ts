ba840610fcc64cc838e7986107e6a8f17ed7c1b7
Alexandre Santos <amenezes031@gmail.com>
2021-07-20 00:27:54 -0300

    read csv file

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,14 @@
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
+import multer from 'multer';
 
-export const router = Router();
+const multerConfig = multer();
+
+const router = Router();
+
+router.post("/products", multerConfig.single("file"), (request: Request, response: Response) => {
+  console.log(request.file?.buffer.toString("utf-8"));
+
+  return response.send();
+});
+
+export { router };
